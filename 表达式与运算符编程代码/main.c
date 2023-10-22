64bebd83681f25b58c91d7975bc71f6405c90ed1
Brian <b22140718@njupt.edn.cn>
2023-10-22 21:24:13 +0800

    Initial Commit

---
+++ 表达式与运算符编程代码/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  表达式与运算符编程代码
+//
+//  Created by Brian on 2023/10/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
